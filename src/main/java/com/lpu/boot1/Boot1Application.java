ad15ad5f69dc9f032723d92762c70bb43e096c84
Bathula Rangaraju <rangafree1@gmail.com>
2026-02-27 18:59:31 +0530

    add file is committed

---
+++ src/main/java/com/lpu/boot1/Boot1Application.java
@@ -0,0 +1,42 @@
+package com.lpu.boot1;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot1Application {
+
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(Boot1Application.class, args);
+		
+		File file=new File("C:/Users/hp/Downloads/Servlet.pdf");
+	PDDocument document=PDDocument.load(file);
+	
+	PDFTextStripper stripper=new PDFTextStripper();
+	int totalPages=document.getNumberOfPages();
+	
+	for(int i=1;i<=totalPages;i++) {
+		
+		stripper.setStartPage(i);
+		stripper.setEndPage(i);
+		
+		String search="while we could";
+		String pageText=stripper.getText(document).toLowerCase();
+		
+		if(pageText.contains(search)) {
+			System.out.println("Found on page: "+i);
+		}
+	}
+	
+	document.close();
+	System.out.println("end");
+	
+	
+	}
+
+}
